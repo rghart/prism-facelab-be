e12fc7a98baf4fa82eb84f446d56b2da9c5aa8f7
Ryan Hart <ryanhart@Caitlins-MBP.hsd1.tn.comcast.net>
2022-07-31 11:53:57 -0500

    Update boulevard auth

---
+++ app/services/boulevard_client.py
@@ -0,0 +1,69 @@
+# WIP - using for testing
+import requests
+
+from app.constants import BOULEVARD_LOCATION_ID
+from app.services.boulevard_auth_constructor import construct_boulevard_auth
+
+endpoint = "https://sandbox.joinblvd.com/api/2020-01/admin"
+gql_query = f"""
+query ListAppointments {{
+  appointments(
+    locationId: "urn:blvd:Location:{BOULEVARD_LOCATION_ID}",
+    first: 100,
+  ) {{
+    edges {{
+      node {{
+        id
+        notes
+
+        client {{
+          firstName
+          lastName
+          email
+          mobilePhone
+        }}
+
+        appointmentServices {{
+          id
+          startAt
+          price
+          duration
+          service {{
+            name
+            category {{
+              name
+            }}
+          }}
+
+          staffRequested
+          staff {{
+            firstName
+            lastName
+            email
+            mobilePhone
+
+            role {{
+              name
+            }}
+          }}
+        }}
+      }}
+    }}
+
+    pageInfo {{
+      endCursor
+      hasNextPage
+    }}
+  }}
+}}
+"""
+basic_auth = construct_boulevard_auth()
+r = requests.post(
+    endpoint,
+    json={"query": gql_query},
+    headers={'Authentication': basic_auth, 'Accept': 'application/json'},
+)
+if r.status_code == 200:
+    print(r.json())
+else:
+    raise Exception(f"Query failed to run with a {r.status_code}. {r.reason}. {r.headers['WWW-Authenticate']}")

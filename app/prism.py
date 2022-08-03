ef22ca68756b626176adb44db908d89118a5a3a3
Ryan Hart <ryanhart@Caitlins-MBP.hsd1.tn.comcast.net>
2022-08-03 00:16:26 -0500

    Create client_created webhook

--- app/prism.py
+++ app/prism.py
@@ -1,9 +1,15 @@
+import logging
+
 from flask import Flask
-from app.webhooks import blvd_appointment_created
+from app.webhooks.blvd import appointment_created, client_created
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('werkzeug').setLevel(logging.INFO)
 
 app = Flask(__name__)
 
-app.register_blueprint(blvd_appointment_created.blueprint)
+app.register_blueprint(appointment_created.blueprint)
+app.register_blueprint(client_created.blueprint)
 
 
 @app.route('/')
